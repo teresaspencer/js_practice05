46d69e331585e556f68d479e7d47f34de0668f96
Teresa Spencer <teresalandkamer@gmail.com>
2025-09-09 14:26:44 -0500

    Refactor functions in main.js for clarity and consistency

--- main.js
+++ main.js
@@ -7,20 +7,32 @@ given in the comments.
 
 // 1) Create a function named helloWorld that returns the exact phrase:
 // "Hello World!".
+function helloWorld() {
+    return "Hello World!";
+}
 
 // 2) Create a function named greeting that will accept a "name" parameter
 // and return the string: "Hello, name." where "name" is replaced
 // with whatever value we want to provide when calling the function.
+function greeting(name) {
+    return "Hello, " + name + ".";
+}
 
 // 3) Create a function named divisibleByThree that accepts a "number" parameter
 // and will return accurate boolean data depending on the value
 // of the "number" parameter. Hint: Try the modulus operator.
+function divisibleByThree(number) {
+    return number % 3 === 0;
+}
 
 // 4) Create a function named averageAge that accepts 3 parameters:
 // num1, num2, and num3. The function should return the average of
 // all three numbers, but the parameter num3 is missing, it should
 // return the average of num1 and num2. Returned results should be
 // rounded to the nearest integer. Hint: Use a Math method to round.
+function averageAge(num1, num2, num3) {
+    return Math.round(Math.average(num1, num2, num3));
+}
 
 // 5) Create a function named leetSpeak that accepts a word
 // as a parameter and returns the same word except all of the letter
@@ -28,3 +40,17 @@ given in the comments.
 // Replace any lower case a with the number 4, too.
 // Hints: You will use a loop. You will use conditionals.
 // Example: leet is returned as l33t. speak is returned as sp34k.
+function leetSpeak(word) {
+    let myString = "";
+    for(let i = 0; i < word.length; i++) {
+        if(word.charAt(i) === 'e') {
+            myString += '3';
+        }
+        else if(word.charAt(i) === 'a') {
+            myString += '4';
+        }
+        else myString += word.charAt(i);
+    }
+    return myString;
+}
+
